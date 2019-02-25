e979bdd2d761a54ae49a6b24191e454c6d6e4608
Tess Guenthner <tessmadeleine@hotmail.com>
2019-02-25 21:37:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function updateObjectWithKeyAndValue(object, key, value) {
  var result = Object.assign({},object,{[key]:value})
  return result
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key]= value
+    return object
+}
+
